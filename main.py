a43981f945c77ce87bd22a0ad54f114cf36fd944
asnewman <ashznewman@gmail.com>
2023-01-03 23:35:18 -0800

    using list to hold state

--- main.py
+++ main.py
@@ -3,104 +3,111 @@ from curses import wrapper
 import logging
 import sys
 
-def setupLogger():
-	logger = logging.getLogger(__file__)
-	hdlr = logging.FileHandler(__file__ + ".log")
-	formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-	hdlr.setFormatter(formatter)
-	logger.addHandler(hdlr)
-	logger.setLevel(logging.DEBUG)
+EDITOR_HEIGHT = 10
+EDITOR_WIDTH = 30
 
-	logger.info("begin")
-	return logger
+logger = logging.getLogger(__file__)
+hdlr = logging.FileHandler(__file__ + ".log")
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.DEBUG)
 
-def main(stdscr):
+logger.info("begin")
+
+def move_down(cursor_y, cursor_x, editor_y, state):
+	logger.debug(f"{cursor_y} {editor_y} {len(state)}")
+
+	# Cursor is at the end of the file
+	# -2 because cursor_y and editor_y both index at 0
+	if cursor_y + editor_y == len(state) - 2:
+		return (cursor_y, cursor_x, editor_y)
+
+	# Cursor is at the end of the screen, scroll down
+	if cursor_y == EDITOR_HEIGHT:
+		return (cursor_y, cursor_x, editor_y + 1)
+
+	# Cursor can just move down
+	return (cursor_y + 1, cursor_x, editor_y)
+
+def main(scr):
+	curses.noecho()
+
 	if len(sys.argv) != 2:
 		exit()
 	file_name = sys.argv[1]
 
 	text_file = open(file_name, "r")
-	state = text_file.read()
+	raw_state = text_file.read()
+	state = raw_state.split("\n")
+	logger.debug(state)
 	text_file.close()
 
-	stdscr.addstr(state)
-	stdscr.refresh()
+	editor = curses.newpad(100, 100)
+	scr.refresh()
 
-	logger = setupLogger()
-
-	curses.noecho()
+	editor_y = 0
+	editor.addstr(raw_state)
+	editor.refresh(editor_y, 0, 0, 0, EDITOR_HEIGHT, EDITOR_WIDTH)
 
 	while True:
-		key = stdscr.getch()
-		y, x = stdscr.getyx()
+		key = scr.getch()
+		y, x = scr.getyx()
 
-		insert_x = x
-		if y > 0:
-			logger.debug("multiple lines detected")
-			# Go to respective y
-			newlines = []
-			for i in range(len(state)):
-				if state[i] == "\n":
-					newlines.append(i)
-			logger.debug(f"len line length: {len(newlines)} y: {y}")
-			if len(newlines) >= y:
-				insert_x = x + newlines[y - 1] + 1
-			else:
-				insert_x = x
+		new_y = y
+		new_x = x
 
-
-		new_y_position = y
-		new_x_position = x
 		logger.debug(f"current cursor position - y: {y} x: {x}")
-
 		logger.debug(key)
 
 		if key == curses.KEY_BACKSPACE:
-			state = state[:insert_x - 1] + state[insert_x:]
-			new_x_position = x - 1
+			state[y] = state[y][:x - 1] + state[y][x:]
+			new_x = x - 1
 		elif key == curses.KEY_LEFT:
-			new_x_position = x - 1
+			new_x = x - 1
 		elif key == curses.KEY_RIGHT:
-			new_x_position = x + 1
+			new_x = x + 1
 		elif key == curses.KEY_UP:
-			new_y_position = y - 1
+			pass
 		elif key == curses.KEY_DOWN:
-			new_y_position = y + 1
+			new_y, new_x, editor_y = move_down(y, x, editor_y, state)
+			logger.debug(f"new_y: {new_y} new_x: {new_x} editor_y: {editor_y}")
 		# Delete line with CTRL + d
 		elif key == 4:
-			state = ""
+			state = []
 		# CTRL + F
 		elif key == 6:
-			key = stdscr.getch()
+			key = scr.getch()
 			# d
 			if key == 100:
 				new_x_position = 0
 				new_y_position = 0
 				state = ""
-				stdscr.clear()
+				scr.clear()
 			# s
 			elif key == 115:
 				text_file = open(file_name, "w")
 				text_file.write(state)
 				text_file.close()
 				exit()
-			stdscr.move(new_y_position, new_x_position)
+			scr.move(new_y_position, new_x_position)
 			continue
 		else:
 			char = chr(key)
 			if char == "\n":
 				logger.debug("new line detected")
-				new_y_position = y + 1
-				new_x_position = 0
+				new_y = y + 1
+				new_x = 0
 			else:
-				new_x_position = x + 1
+				new_x = x + 1
 
-			state = state[:insert_x] + char + state[insert_x:]
+			state[y] = state[y][:x] + char + state[x:]
 
-		stdscr.clear()
-		stdscr.addstr(state)
-		logger.debug(f"new locations - y: {new_y_position} x: {new_x_position}")
-		stdscr.move(new_y_position, new_x_position)
-		stdscr.refresh()
+		editor.clear()
+		editor.addstr("\n".join(state))
+		editor.refresh(editor_y, 0, 0, 0, EDITOR_HEIGHT, EDITOR_WIDTH)
+		logger.debug(f"new locations - y: {new_y} x: {new_x}")
+		scr.move(new_y, new_x)
+		scr.refresh()
 
 wrapper(main)
